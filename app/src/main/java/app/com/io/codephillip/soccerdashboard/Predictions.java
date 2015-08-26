e4acbd8ac8d484c67435478af453a9b482788954
codephillip <codephillip@gmail.com>
2015-08-26 17:42:14 +0300

    added ArrayAdapter to the listview in predictions

--- app/src/main/java/app/com/io/codephillip/soccerdashboard/Predictions.java
+++ app/src/main/java/app/com/io/codephillip/soccerdashboard/Predictions.java
@@ -6,15 +6,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Predictions extends Fragment {
+    private ListView predictionList;
+    private String[] predictionArray;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater,
 			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.predictions_layout, container, false);
+		View view = inflater.inflate(R.layout.predictions_layout, container, false);
 
-		return rootView;
+        predictionList = (ListView) view.findViewById(R.id.lv_predictions);
+
+        predictionArray = new String[]{
+                "Man-U","Chelsea","Arsenal","Spurs","Leicester City", "Liverpool FC", "Crystal Palace","Man-U","Chelsea","Arsenal","Spurs","Leicester City", "Liverpool FC", "Crystal Palace"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, android.R.id.text1, predictionArray
+                );
+
+        predictionList.setAdapter(adapter);
+
+		return view;
 	}
 
 
