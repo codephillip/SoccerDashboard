854181d0e9c315ffdaf1f52a5f16057cfef988d9
codephillip <codephillip@gmail.com>
2015-08-26 16:58:54 +0300

    added adapters to listview in fixtures and tables

--- app/src/main/java/app/com/io/codephillip/soccerdashboard/Fixtures.java
+++ app/src/main/java/app/com/io/codephillip/soccerdashboard/Fixtures.java
@@ -6,13 +6,30 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Fixtures extends Fragment {
+    private ListView fixtureList;
+    private String[] fixtureArray;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater,
 			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fixtures_layout, container, false);
+
+        fixtureList = (ListView) view.findViewById(R.id.lv_fixtures);
+
+        fixtureArray = new String[]{
+                "Man-U","Chelsea","Arsenal","Spurs","Leicester City", "Liverpool FC", "Crystal Palace","Man-U","Chelsea","Arsenal","Spurs","Leicester City", "Liverpool FC", "Crystal Palace"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1,
+                android.R.id.text1, fixtureArray
+                );
+
+        fixtureList.setAdapter(adapter);
+
 		return view;
 	}
 }
--- app/src/main/java/app/com/io/codephillip/soccerdashboard/Tables.java
+++ app/src/main/java/app/com/io/codephillip/soccerdashboard/Tables.java
@@ -6,13 +6,29 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Tables extends Fragment {
+    private String[] tableArray;
+    private ListView tableList;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater,
 			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.tables_layout, container, false);
+
+        tableList = (ListView) view.findViewById(R.id.lv_tables);
+
+        tableArray = new String[]{
+                "Man-U","Chelsea","Arsenal","Spurs","Leicester City", "Liverpool FC", "Crystal Palace","Man-U","Chelsea","Arsenal","Spurs","Leicester City", "Liverpool FC", "Crystal Palace"
+        };
+
+        ArrayAdapter <String>  adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, android.R.id.text1, tableArray
+                );
+        tableList.setAdapter(adapter);
+
 		return view;
 	}
 }
