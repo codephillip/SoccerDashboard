b47f501d2439ef8a6a2caa533783a4526afbb670
codephillip <codephillip@gmail.com>
2015-08-26 17:47:36 +0300

    added intentService

---
+++ app/src/main/java/app/com/io/codephillip/soccerdashboard/services/ApiIntentService.java
@@ -0,0 +1,16 @@
+package app.com.io.codephillip.soccerdashboard.services;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+
+public class ApiIntentService extends IntentService {
+
+    public ApiIntentService() {
+        super("ApiIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+    }
+}
